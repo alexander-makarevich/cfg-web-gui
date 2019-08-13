37fe8e4f565ce2778c7d7c1b3ab5e02f644e99e1
Alexander S. Makarevich <alexander.makarevich@eltex.loc>
2019-08-13 20:47:12 +0700

    refs #127416: group form controls and add async uniq label validator

--- src/app/inventory/edit-as-draft-dialog/edit-as-draft-dialog.component.ts
+++ src/app/inventory/edit-as-draft-dialog/edit-as-draft-dialog.component.ts
@@ -1,8 +1,9 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {ChangeDetectorRef, Component, Inject, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 
 import {Configuration, InventoryService, ShortDraft} from '../inventory.service';
+import {of} from 'rxjs';
 
 export enum DialogAction {
   Cancel = 1,
@@ -10,36 +11,48 @@ export enum DialogAction {
   SaveAndUpload,
 }
 
+export const uniqueLabelIpPairValidator = (service: InventoryService, changeRef: ChangeDetectorRef) => {
+  return (control: FormGroup) => {
+    return of({uniqueLabelIpPair: true});
+  };
+};
+
 @Component({
   selector: 'app-edit-as-draft-dialog',
   templateUrl: './edit-as-draft-dialog.component.html',
   styleUrls: ['./edit-as-draft-dialog.component.scss']
 })
 export class EditAsDraftDialogComponent implements OnInit {
-  ip: string;
-  label = new FormControl('', Validators.required);
-  annotation = new FormControl('');
-  content = new FormControl('', Validators.required);
+  formGroup: FormGroup = new FormGroup({
+    ip: new FormControl(''),
+    label: new FormControl('', [Validators.required]),
+    annotation: new FormControl(''),
+    content: new FormControl('', Validators.required),
+  }, [], [uniqueLabelIpPairValidator(this.service, this.changeRef)]);
 
   constructor(public dialogRef: MatDialogRef<EditAsDraftDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public configurations: Configuration[],
-              public service: InventoryService) {
+              public service: InventoryService,
+              private changeRef: ChangeDetectorRef) {
   }
 
   ngOnInit() {
-    this.ip = this.configurations[0].ip;
+    window['fg'] = this.formGroup;
     const commands = this.configurations[0].commands;
-    this.content.setValue(commands.join('\n'));
+    this.formGroup.patchValue({
+      ip: this.configurations[0].ip,
+      content: commands.join('\n'),
+    });
   }
 
   save() {
-    const shortDraft: ShortDraft = {ip: this.ip, label: this.label.value, annotation: this.annotation.value, content: this.content.value};
+    const shortDraft: ShortDraft = {ip: this.formGroup.get('ip').value, label: this.formGroup.get('label').value, annotation: this.formGroup.get('annotation').value, content: this.formGroup.get('content').value};
     this.service.saveDraft(shortDraft);
     this.dialogRef.close();
   }
 
   saveAndUpload() {
-    const shortDraft: ShortDraft = {ip: this.ip, label: this.label.value, annotation: this.annotation.value, content: this.content.value};
+    const shortDraft: ShortDraft = {ip: this.formGroup.get('ip').value, label: this.formGroup.get('label').value, annotation: this.formGroup.get('annotation').value, content: this.formGroup.get('content').value};
     this.service.uploadDraft(shortDraft);
     this.dialogRef.close(DialogAction.SaveAndUpload);
   }
