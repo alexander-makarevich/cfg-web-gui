71bfc33bb57a676509d85fa738f55b8a493f14b8
alexander.makarevich <alexander.makarevich@eltex.loc>
2019-08-15 18:32:55 +0700

    refs #127655: add selection for drafts

--- src/app/inventory/drafts/drafts.component.ts
+++ src/app/inventory/drafts/drafts.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {MatTableDataSource} from '@angular/material';
 import {Draft, DRAFT_DATA} from '../inventory.service';
+import {SelectionModel} from "@angular/cdk/collections";
 
 @Component({
   selector: 'app-drafts',
@@ -8,8 +9,31 @@ import {Draft, DRAFT_DATA} from '../inventory.service';
   styleUrls: ['./drafts.component.scss']
 })
 export class DraftsComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'ip', 'labels', 'author', 'updateTime'];
+  displayedColumns: string[] = ['select', 'id', 'ip', 'labels', 'author', 'updateTime'];
   dataSource = new MatTableDataSource<Draft>(DRAFT_DATA);
+  selection = new SelectionModel<Draft>(true, []);
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: Draft): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.ip}`;
+  }
 
   constructor() { }
 
